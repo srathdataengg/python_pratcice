e4414788f3544270037a56ecbd0c42ba3a5e171b
srathdataengg <srathdataengineer@gmail.com>
2023-12-21 14:33:58 +0530

    sample files

---
+++ 30Questions/2ndQuestion.py
@@ -0,0 +1,12 @@
+"""
+Write a program to perform different arithmetic operations in Python
+"""
+a=int(input("Enter a value"));
+b=int(input("Enter b value"));
+print("addition of a + b :-",a+b)
+print("Substraction of a -b :-",a-b)
+print("multipplication of a * b:-",a*b)
+print("Divison of a / b :-",a/b)
+print("Remainder of a %b :-",a%b)
+print("Exponent of a and b",a**b)
+print("Floar division of a and b",a//b)
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("let's test your python knowledge")
+print("******************")
+print("welcome to vscode")

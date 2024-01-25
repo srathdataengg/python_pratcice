a9a1164fef1f07cda3cd4324dfa9270e42e95320
srathdataengg <srathdataengineer@gmail.com>
2024-01-25 15:15:01 +0530

    q04 and q05

--- websites/tutorsjoe/Question04.py
+++ websites/tutorsjoe/Question04.py
@@ -1,3 +1,22 @@
 """
-
+Write a python program to get the smallest number from a list.
+item = [51,7,3,10,8]
 """
+
+item = [51, 7, 3, 10, 8]
+
+min_value = item[0]
+
+for i in item:
+    if i < min_value:
+        min_value = i
+print(min_value)
+
+# Approach -2
+
+item1 = [2, 3, 4, 5, 6]
+print(item1)
+item1.sort()
+print(item1)
+
+print("minmum value :-", item1[0])
